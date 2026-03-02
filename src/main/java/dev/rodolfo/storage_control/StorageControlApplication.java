78e37ba20196a68a70f1ff1f56f7a50947b55e2a
Rodolfo Gama <rodolfo.gama@season.com.br>
2026-03-02 16:31:22 -0300

    First Commit

---
+++ src/main/java/dev/rodolfo/storage_control/StorageControlApplication.java
@@ -0,0 +1,13 @@
+package dev.rodolfo.storage_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rodolfo/storage_control/StorageControlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rodolfo.storage_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
